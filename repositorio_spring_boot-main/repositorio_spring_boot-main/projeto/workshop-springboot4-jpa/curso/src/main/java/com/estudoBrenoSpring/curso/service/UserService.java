2e6cbe9cdea025c5869bd2c59296d1b1f09dc3bd
Breno Nunes <166417202+brenonun3s@users.noreply.github.com>
2024-12-27 18:17:27 -0300

    Add files via upload
    projeto atualizado. inserido tratamento de exceções, metodos de inserir, deletar e atualizar
    
    pendente : configurar para dev e mesclar com o banco de dadosPostgreSQL

---
+++ repositorio_spring_boot-main/repositorio_spring_boot-main/projeto/workshop-springboot4-jpa/curso/src/main/java/com/estudoBrenoSpring/curso/service/UserService.java
@@ -0,0 +1,64 @@
+package com.estudoBrenoSpring.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.estudoBrenoSpring.curso.entities.User;
+import com.estudoBrenoSpring.curso.repositories.UserRepository;
+import com.estudoBrenoSpring.curso.service.exceptions.DataBaseException;
+import com.estudoBrenoSpring.curso.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+	}
+
+	public User uptade(Long id, User obj) {
+		try {
+		User entity = repository.getReferenceById(id);
+		uptadeData(entity, obj);
+		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void uptadeData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
+}
